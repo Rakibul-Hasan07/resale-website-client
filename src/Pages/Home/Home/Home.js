7197064aa702a056261b3ed731a789167e6aadb1
shihab00027 <shihabhasan981@gmail.com>
2022-11-27 21:23:25 +0600

    readme file added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import Navbar from '../../Shared/Navbar/Navbar';
-import AdvertisedItems from '../AdvertisedItems/AdvertisedItems';
 import Carousel from '../Carousel/Carousel';
 import OfferBanner from '../OfferBanner/OfferBanner';
 
@@ -9,7 +6,6 @@ const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
-            <AdvertisedItems></AdvertisedItems>
             <OfferBanner></OfferBanner>
         </div>
     );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -85,26 +85,26 @@ const Register = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo</span>
                                 </label>
-                                <input type="file" name='image' placeholder="name" className="input" />
+                                <input type="file" name='image' placeholder="name" className="input" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
 
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
