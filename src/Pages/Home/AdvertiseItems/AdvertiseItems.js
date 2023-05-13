979aae17d3a37bbde0f1e75bc4ad69347e12cb76
rakibul <rk.rakibulhasan07@gmail.com>
2023-05-13 12:13:22 +0600

    cart update

--- src/Pages/Home/AdvertiseItems/AdvertiseItems.js
+++ src/Pages/Home/AdvertiseItems/AdvertiseItems.js
@@ -3,11 +3,15 @@ import AdvertiseItemsCard from './AdvertiseItemsCard';
 
 const AdvertiseItems = () => {
     const [advertiseData, setAdvertiseData] = useState([])
+
     useEffect(() => {
         fetch('https://resale-website-server.vercel.app/add-advertise')
             .then(res => res.json())
             .then(data => setAdvertiseData(data))
+
+
     }, [])
+    
     return (
         <div>
             {advertiseData && <h3 className='text-4xl font-bold text-center mt-10'>Advertise Items</h3>}
--- src/Pages/Home/AdvertiseItems/AdvertiseItemsCard.js
+++ src/Pages/Home/AdvertiseItems/AdvertiseItemsCard.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { AiFillStar, AiOutlineShoppingCart } from 'react-icons/ai';
+import { FaLocationArrow } from 'react-icons/fa';
+import PrimaryBtn from '../../../Components/PrimaryBtn/PrimaryBtn';
 
 const AdvertiseItemsCard = ({ advertise }) => {
     const { productName, price, image, quality, location, year } = advertise;
     return (
         <div>
-            <div className="card bg-blue-600 shadow-xl lg:h-[450px]">
+            <div className="card dark:bg-slate-500 shadow-xl lg:h-[450px]">
                 <figure><img className='p-5' src={image} alt="" /></figure>
-                <div className="card-body text-center text-xl font-bold">
+                <div className="card-body text-xl font-bold">
+
                     <h2 className="card-title text-2xl font-bold">{productName}</h2>
-                    <p>Price:$ {price}</p>
-                    <p>Quality: {quality}</p>
-                    <p>Location: {location}</p>
-                    <p>Purchase Year: {year}</p>
+                    <span className='flex items-center justify-between'>
+                        <p className='flex'>
+                            <AiFillStar className='text-orange-600'></AiFillStar>
+                            <AiFillStar className='text-orange-600'></AiFillStar>
+                            <AiFillStar className='text-orange-600'></AiFillStar>
+                            <AiFillStar className='text-orange-600'></AiFillStar>
+                        </p>
+                        <p className='bg-blue-400 text-center'>{quality}</p>
+                    </span>
+                    <p>$ {price}</p>
+                    <span className='flex justify-center items-center'>
+                        <p className='flex items-center'> <FaLocationArrow></FaLocationArrow> {location}</p>
+                        <p className='flex items-center'> <AiOutlineShoppingCart></AiOutlineShoppingCart> {year}</p>
+                    </span>
                 </div>
+                <span className='text-center mb-6'>
+                    <PrimaryBtn>Buy Now</PrimaryBtn>
+                </span>
             </div>
         </div>
     );
